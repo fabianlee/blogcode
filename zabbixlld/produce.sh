41bc25bbd35410e352787d9de0a82dba405d3abe
flee999@hotmail.com <flee999@hotmail.com>
2017-05-29 13:29:54 -0500

    collateral for zabbix lld

---
+++ zabbixlld/produce.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# send back discovery key, list of all available array keys
+# for a discovery type of "Zabbix agent"
+cat << EOF
+{ "data": [
+  { "{#ITEMNAME}":"carrot" },
+  { "{#ITEMNAME}":"banana" },
+  { "{#ITEMNAME}":"lettuce" },
+  { "{#ITEMNAME}":"tomato" }
+]}
+EOF
+
+# now take advantage of this invocation to send back values
+# build up list of values in /tmp/zdata.txt
+agenthost="`hostname -f`"
+zserver="192.168.2.116"
+zport="10051"
+
+cat /dev/null > /tmp/zdata.txt
+for item in "carrot" "banana" "lettuce" "tomato"; do
+  randNum="$(( (RANDOM % 30)+1 ))"
+  echo $agenthost instock[$item] $randNum >> /tmp/zdata.txt
+done
+
+# push all these trapper values back to zabbix
+zabbix_sender -vv -z $zserver -p $zport -i /tmp/zdata.txt >> /tmp/zsender.log 2>&1
