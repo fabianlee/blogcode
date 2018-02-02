54ee72e0e2e5f89b502f7d2418aa688462c68178
flee999 <flee999@hotmail.com>
2018-02-01 21:41:41 -0500

    removed xmladaptertest, moving to main level

--- xmladaptertest/src/main/java/org/fabianlee/xmladaptertest/domain/package-info.java
+++
@@ -1,7 +0,0 @@
-@javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters
-({
-    @javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter(value=org.fabianlee.xmladaptertest.adapters.AvatarIconAdapter.class,type=org.fabianlee.xmladaptertest.domain.AvatarIcon.class)
-})
-
-package org.fabianlee.xmladaptertest.domain;
-
