01c4e78096d0f5ba6d8d2a5a6aefa2ef6f3806f0
flee999 <flee999@hotmail.com>
2018-10-14 17:28:27 -0400

    headless chrome ruby selenium test

---
+++ ruby/headless_chrome.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/ruby
+require 'selenium-webdriver'
+
+def setup
+
+    # path to Chrome driver
+    Selenium::WebDriver::Chrome.driver_path = "./chromedriver"
+
+    # options
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+
+    # capabilities
+    caps = Selenium::WebDriver::Remote::Capabilities.new
+    caps["screen_resolution"] = "600x768"
+    caps["record_network"] = "true"
+    #options.add_argument('--disable-gpu')
+    #options.add_argument('--remote-debugging-port=9222')
+
+    # create driver using options and capabilities
+    @driver = Selenium::WebDriver.for :chrome, options: options, desired_capabilities: caps
+
+end
+
+def teardown
+    @driver.quit
+end
+
+def run
+    setup
+    yield
+    teardown
+end
+
+
+############ MAIN ###################
+
+run do
+
+    # go to google search page
+    @driver.get 'http://www.google.com'
+
+    # do search
+    element = @driver.find_element(:name, 'q')
+    element.send_keys "ruby selenium webdriver"
+    element.submit
+
+    # create screenshot of search results
+    print "RESULTS TITLE: #{@driver.title}\n"
+    @driver.save_screenshot('headless.png')
+end
