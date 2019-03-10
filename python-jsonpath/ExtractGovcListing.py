0fcbfcf7d36f108d20291a4c45ef6db991f49c6e
flee999 <flee999@hotmail.com>
2019-03-10 13:23:07 -0400

    parsing a govc output listing for basic info on vcenter vm

---
+++ python-jsonpath/ExtractGovcListing.py
@@ -0,0 +1,85 @@
+#
+# Parses output of "govc ls -l -json <vmpath>"
+# and extracts basic information on cpu,mem,disk,network
+#
+# PREREQUISITES:
+#   jsonpath-rw and jsonpath-rw-ext modules
+#
+#   virtualenv jsonpath
+#   source jsonpath/bin/activate
+#   pip install jsonpath-rw jsonpath-rw-ext
+#
+
+import sys
+import os.path
+import json
+
+from jsonpath_rw import jsonpath
+#from jsonpath_rw import parse
+# override parse for more capabilities
+from jsonpath_rw_ext import parse
+
+import jsonpath_rw_ext as jp
+
+# credit for this function goes to Dave
+# https://stackoverflow.com/questions/33750233/convert-cidr-to-subnet-mask-in-python#43904598
+#
+def cidr_to_netmask(cidr):
+  cidr = int(cidr)
+  mask = (0xffffffff >> (32 - cidr)) << (32 - cidr)
+  return (str( (0xff000000 & mask) >> 24) + '.' +
+          str( (0x00ff0000 & mask) >> 16) + '.' +
+          str( (0x0000ff00 & mask) >> 8)  + '.' +
+          str( (0x000000ff & mask)))
+
+
+####### MAIN ##########################################################3
+
+# argument check
+if len(sys.argv)<=1:
+  print("Expected 1 arguments, found {}".format(len(sys.argv)-1))
+  print("USAGE: govcJSONFile")
+  print("EXAMPLE: myvm.json")
+  exit(1)
+filename = sys.argv[1]
+
+# validate file existence
+if not os.path.isfile(filename):
+  print("ERROR could not find json file with govc output {}".format(filename))
+  exit(2)
+
+# read file from disk
+json_file = open("/home/fabian/Documents/govc.json")
+json_data=json.load(json_file)
+
+# name of VM
+print("name: {}".format( jp.match1("elements[*].Object.Config.Name",json_data) ))
+print("OS: {}".format( jp.match1("elements[*].Object.Guest.GuestFullName",json_data) ))
+
+# get path to vm, break apart
+fullPath=jp.match1("elements[*].Path",json_data)
+fullPath=fullPath[0:fullPath.rfind("/")]
+pathsplit=fullPath.split("/")
+print("Full path: {}".format( fullPath ))
+print("Parent Folder: {}".format( pathsplit[len(pathsplit)-1] ))
+
+# show IP address and cidr/netmask
+print("Default IpAddress: {}".format( jp.match1("elements[*].Object.Guest.IpAddress",json_data) ) )
+for IpAddress in jp.match("elements[*].Object.Guest.Net[*].IpConfig.IpAddress[*]",json_data):
+  cidr=IpAddress['PrefixLength'] 
+  print("IP {} with cidr {} and netmask {}".format( IpAddress['IpAddress'],cidr,cidr_to_netmask(cidr)  ))
+
+# memory/cpu
+#print("MaxMemoryUsage: {}".format( jp.match1("elements[*].Object.Runtime.MaxMemoryUsage",json_data) ) )
+print("MemorySizeMB: {}".format( jp.match1("elements[*].Object.Summary.Config.MemorySizeMB",json_data) ) )
+print("CPU: {}".format( jp.match1("elements[*].Object.Summary.Config.NumCpu",json_data) ) )
+
+# disks
+for disk in jp.match("elements[*].Object.Config.Hardware.Device[?CapacityInKB>0]",json_data):
+  capacityGB = (disk['CapacityInKB']/1024/1024)
+  filestore = disk['Backing']['FileName'].split(' ')
+  print("DISK capacity is {}Gb on {}".format( capacityGB,filestore[0] ))
+
+# OS mounts
+#print("Guest.Disk: {}".format( jp.match1("elements[*].Object.Guest.Disk",json_data) ) )
+
