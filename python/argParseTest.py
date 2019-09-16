13931732832cb37e33cf4daba5b8125ce6a7319f
flee999 <flee999@hotmail.com>
2019-09-16 19:52:06 -0400

    choices and optional flag

--- python/argParseTest.py
+++ python/argParseTest.py
@@ -13,13 +13,14 @@ import argparse
 
 
 # perform math operation, by default addition
-def showMathResult(a,b,op="add"):
+def showMathResult(a,b,op="add",upper=False):
   res = 0
   if op=="mul":
     res= a * b
   else:
     res = a + b
-  print("{} {} {} = {}".format(a,op,b,res))
+  opDisplay = op.upper() if upper else op
+  print("{} {} {} = {}".format(a,opDisplay,b,res))
 
 
 def main(argv):
@@ -28,13 +29,14 @@ def main(argv):
   ap = argparse.ArgumentParser(description="Example using ArgParse library")
   ap.add_argument('a',type=int,help="first integer")
   ap.add_argument('b',type=int,help="second integer")
-  ap.add_argument('-o','--op',default="add",help="operation=add|mul")
+  ap.add_argument('-o','--op',default="add",choices=['add','mul'],help="operation=add|mul")
+  ap.add_argument('-u','--upper',action="store_true",help="show uppercase")
 
   # parse args
   args = ap.parse_args()
 
   # print results of math operation
-  showMathResult(args.a,args.b,args.op)
+  showMathResult(args.a,args.b,args.op,args.upper)
 
 
 
