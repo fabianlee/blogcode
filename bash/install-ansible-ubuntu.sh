946f296f862a9a66f05d49bf1326ba7edc60638f
flee999 <flee999@hotmail.com>
2020-11-24 14:48:34 -0500

    apt install

--- bash/install-ansible-ubuntu.sh
+++ bash/install-ansible-ubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-#sudo apt-get update
-#sudo apt-get install software-properties-common -y
-#sudo -E apt-add-repository --yes --update ppa:ansible/ansible
-#sudo apt-get install ansible -y
+sudo apt-get update
+sudo apt-get install software-properties-common -y
+sudo -E apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install ansible -y
 
 # quick ansible config
 cat << EOF > ansible.cfg
