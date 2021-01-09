4154f4e4c43cd8ffd22aae0f5ef2a49a0290fecd
flee999 <flee999@hotmail.com>
2021-01-09 13:50:52 -0500

    grep lookahead testing

---
+++ bash/grep_lookahead_lookbehind.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Tests LookAhead and LookBehind with grep to isolate output
+#
+
+testfile="greptest.txt"
+
+# create test file
+cat > $testfile << EOF
+http://www.google.com?q=foo
+https://images.google.com
+https://wikipedia.org/wiki/Regular_expression
+https://fr.wikipedia.org/wiki/Regular_expression
+http://www.yahoo.com
+EOF
+
+
+echo "#############################"
+echo "plain grep to pull out secure and insecure URL"
+grep --color -E 'https?://([^ /?\"])*' $testfile
+
+echo ""
+echo "#############################"
+echo "same regex, but with -o output flag"
+grep --color -Eo 'https?://([^ /?\"])*' $testfile
+
+echo ""
+echo "#############################"
+echo "PCRE Perl regex with \K which resets match to simulate LookBehind"
+grep -Po 'https?://\K([^ /?\"])*' $testfile
+
+echo ""
+echo "#############################"
+echo "PCR Perl regex with static length LookBehind"
+grep -Po "(?<=https://)([^ /?\"])*" $testfile
+
+echo ""
+echo "#############################"
+echo "PCR Perl regex with brute forced variable length LookBehind, use /K instead"
+grep -Po "(?:(?<=http://)|(?<=https://))([^ /?\"])*" $testfile
+
+echo ""
+echo "#############################"
+echo "PCR Perl regex with LookAhead for .org domains"
+grep -Po "https?://\K([^ /?\"])*(?=.com)" $testfile
+
+
+# cleanup
+rm $testfile
+
