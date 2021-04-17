b487ecfc9248f67df34edb54bacb263926facd3b
flee999 <flee999@hotmail.com>
2021-04-17 16:10:37 -0400

    added substitution with exclusions

--- bash/test_chain_sed.sh
+++ bash/test_chain_sed.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 #
 # chain 2 sed substitutions for output
+# https://fabianlee.org/2021/03/25/bash-performing-multiple-substitutions-with-a-single-sed-invocation/
 #
+# substitution with exclusion pattern
+# https://fabianlee.org/2021/04/17/bash-sed-substitution-with-an-exclusion-pattern/
+#
+
+echo "***chained substitutions separated by semicolon..."
 
 sed "s/hello/goodbye/g; s/quick/slow/g" <<EOF
 hello, world!
+hello, universe!
+the quick brown fox
+EOF
+
+echo ""
+echo "***substitution preceded by exclusion..."
+
+sed "/\(world\|galaxy\)/! s/hello/goodbye/g; s/quick/slow/g" <<EOF
+hello, world!
+hello, galaxy!
 hello, universe!
 the quick brown fox
 EOF
