ff920f2d59d4904f5c568ca1c665942d4547976a
flee999 <flee999@hotmail.com>
2021-05-24 09:22:22 -0400

    add DEBUG logging to spot issues

--- vagrant/awscli1604/list_aws_regions.py
+++ vagrant/awscli1604/list_aws_regions.py
@@ -1,10 +1,15 @@
-#
+# 
 # Runs smoke test on AWS SDK for Python
 # list regions
 #
 
 # boto3 library
-import boto3
+import boto3, botocore
+import logging
+
+# full debug logging so we can see any issues
+logging.basicConfig(level=logging.DEBUG,format=f'%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger()
 
 # create client
 ec2 = boto3.client('ec2')
