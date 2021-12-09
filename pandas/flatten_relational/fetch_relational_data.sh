d4accbbbfc1c172b89dc1c597b3de4d15d1477dd
flee999 <flee999@hotmail.com>
2021-12-09 13:05:09 -0500

    only retrieve necessary csv dump files from github

--- pandas/flatten_relational/fetch_relational_data.sh
+++ pandas/flatten_relational/fetch_relational_data.sh
@@ -2,15 +2,16 @@
 
 echo "Fetching relevant csv datafiles from Microsoft AdventureWorks relational database..."
 
-for f in StateProvince.csv Address.csv BusinessEntity.csv SalesOrderHeader.csv Address.csv CreditCard.csv instawdb.sql; do
-  [ -f $f ] || wget https://github.com/microsoft/sql-server-samples/raw/master/samples/databases/adventure-works/oltp-install-script/$f
+for f in StateProvince.csv SalesOrderHeader.csv Address.csv CreditCard.csv instawdb.sql; do
+  [ -f $f ] || wget -q https://github.com/microsoft/sql-server-samples/raw/master/samples/databases/adventure-works/oltp-install-script/$f
 done
 
-# convert from utf-16 to utf-8 for parsing
-iconv -f utf-16 -t utf-8 BusinessEntity.csv -o BusinessEntity8.csv
-# cleanup multi-character separator
-cat BusinessEntity8.csv | tr -d "+" | sed  's/&\|$//' > BusinessEntity-clean.csv
+# some files are in utf-16 with multi-char separator
+# they would be cleaned like this
+#iconv -f utf-16 -t utf-8 BusinessEntity.csv -o BusinessEntity8.csv
+#cat BusinessEntity8.csv | tr -d "+" | sed  's/&\|$//' > BusinessEntity-clean.csv
 
+# convert from utf-16 to utf-8 for proper parsing by pandas
 iconv -f utf-16 -t utf-8 StateProvince.csv -o StateProvince8.csv
 
 ls -l *.csv
