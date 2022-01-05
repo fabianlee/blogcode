dab868ec9edf37a242a917fe465e1a1e204aaaba
flee999 <flee999@hotmail.com>
2022-01-05 06:51:32 -0500

    added demo of 'envsubst' utility to peform same action

--- bash/test_generic_variables_template.sh
+++ bash/test_generic_variables_template.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 #
-# using sed to replace multiple variables in template
+# using sed or envsubst to replace multiple variables in template
 # blog: https://fabianlee.org/2021/05/29/bash-render-template-from-matching-bash-variables/
 #
 
 # declare variables
 first="1"
 animal="dog"
+
+echo ""
+echo "##########"
+echo "Dollar sign templating style"
+echo "##########"
 
 # create template, heredoc without var evaluation
 read -r -d '' mytemplate <<'EOF'
@@ -23,10 +28,11 @@ done
 sed -e "$sedcmd" <(echo "$mytemplate")
 
 
-echo "##########"
-echo "Alternative templating style"
-echo "##########"
 
+echo ""
+echo "##########"
+echo "Dollar sign with curly bracket style"
+echo "##########"
 
 # allow var evaluation, so escape dollar sign
 read -r -d '' mytemplate <<EOF
@@ -42,3 +48,15 @@ for var in first animal ; do
 done
 sed -e "$sedcmd" <(echo "$mytemplate")
 
+
+echo ""
+echo "##########"
+echo "Using envsubst"
+echo "##########"
+
+export first
+export animal
+# only substitute listed variables
+#echo "$mytemplate" | envsubst '$first $animal'
+# substitute all env variables
+echo "$mytemplate" | envsubst
