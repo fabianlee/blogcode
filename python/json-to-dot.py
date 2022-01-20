651b170e4b2bcfe789e6948a7cd6376aefc63391
flee999 <flee999@hotmail.com>
2022-01-20 16:53:01 -0500

    now handles empty dict and array

--- python/json-to-dot.py
+++ python/json-to-dot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # originally from https://techtldr.com/convert-json-to-dot-notation-with-python/
+# enhanced with ability to handle empty dict and array
 #
 # Takes json and produces dot notation paths
 #
@@ -10,11 +11,17 @@ import sys
 
 def getKeys(val, old=""):
     if isinstance(val, dict):
-        for k in val.keys():
+        if val:
+          for k in val.keys():
             getKeys(val[k], old + "." + str(k))
+        else:
+          print("{} : {}".format(old,"{}"))
     elif isinstance(val, list):
-        for i,k in enumerate(val):
+        if val:
+          for i,k in enumerate(val):
             getKeys(k, old + "." + str(i))
+        else:
+          print("{} : []".format(old,"{}"))
     else:
         print("{} : {}".format(old,str(val)))
 
