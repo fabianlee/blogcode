29edcfda7438dded040113d69385994e66affd09
flee999 <flee999@hotmail.com>
2022-01-20 16:51:46 -0500

    pull cert from URL and then parse for brief content

---
+++ bash/pull_and_parse_certs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Pulls all PEM certs out of yaml file
+# then examines properties using openssl
+
+echo | openssl s_client -showcerts -servername $1 -connect $1:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > $1.crt
+
+# pull out multiline sections, remove leading spaces
+sed -ne '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' $1.crt | sed 's/^\s*//' > allcerts.pem
+
+# count how many certs were pulled
+certcount=$(grep -e "-----BEGIN CERTIFICATE-----" allcerts.pem | wc -l)
+
+# pull each cert individually, use openssl to show critical properties
+for index in $(seq 1 $certcount); do
+  echo "==== cert $index"
+  awk "/-----BEGIN CERTIFICATE-----/{i++}i==$index" allcerts.pem > $index.crt
+  openssl x509 -in $index.crt -text -noout | grep -E "Subject:|Not After :|DNS:"
+  rm $index.crt
+done
+
+rm allcerts.pem
