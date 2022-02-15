d2aa6ac2a2267a17aceb22a1d2c0cc6804a4be45
flee999 <flee999@hotmail.com>
2022-02-14 22:45:01 -0500

    optionally checks CA cert and cert for pairing

--- bash/openssl_key_cert_check.sh
+++ bash/openssl_key_cert_check.sh
@@ -3,10 +3,16 @@
 # validates if key and cert are matched
 #
 
+[[ -n "$1" && -n "$2" ]] || { echo "Usage: keyFile certFile [CACertFile]"; exit 1; }
+
 openssl rsa -noout -modulus -in $1 | openssl md5
 openssl x509 -noout -modulus -in  $2 | openssl md5
-
 echo "if the md5 matches, the key matches the cert"
+
+if [ -n "$3" ]; then
+  echo "Check if CA and cert are paired"
+  openssl verify -CAfile $3 $2
+fi
 
 # check info in csr
 #openssl req -text -noout -verify -in my.csr
