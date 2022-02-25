4b801a760ed567bd536454f1c1ce5939a9c43001
flee999 <flee999@hotmail.com>
2022-02-24 20:16:30 -0500

    example of traps in BASH

--- bash/test_trap.sh
+++ bash/test_trap.sh
@@ -1,64 +1,54 @@
 #!/bin/bash
 #
-# Illustrates use of KILL and SIGINT capture in BASH
+# Illustrates use of SIGINT, SIGUSR1, SIGEXIT capture in BASH
 #
 
-function pingtimeout_kill() {
-  echo "KILLED. timeout from ping"
+# custom user signal
+sigusr1_count=0
+function sigusr1_capture() {
+  ((sigusr1_count+=1))
+  echo "SIGUSR1 called $sigusr1_count times"
 }
 
+# Ctrl-C signal
 ctrl_c_count=0
 function sigint_capture() {
-  $((ctrl_c_count+=1))
-  echo "CTRL-C sensed for $ctrl_c_count time"
+  ((ctrl_c_count+=1))
+  echo "SIGINT CTRL-C sensed for $ctrl_c_count time"
   if [ $ctrl_c_count -gt 2 ]; then
-    echo "stopping loop because CTRL-C pressed 3 times"
+    echo "SIGINT Ctrl-C pressed 3 times, flipping flag"
     continue_looping=0
   fi
+}
+
+# signal on exit of main process
+function sigexit_capture() {
+  echo ""
+  echo "== FINAL EXIT COUNTS ==="
+  echo "SIGUSR1 called $sigusr1_count times"
+  echo "SIGINT CTRL-C sensed for $ctrl_c_count time"
 }
 
 
 ########## MAIN #################
 
-# trap 'SIGINT' <Ctrl>-C
-#trap sigint_capture SIGINT
+# trap signals to functions
+trap sigint_capture SIGINT
+trap sigusr1_capture USR1
+trap sigexit_capture EXIT
 
-#continue_looping=1
-#while [ $continue_looping -eq 1 ]; do
-#  echo "Waiting for you to press <CTRL>-C 3 times..."
-#  sleep 1
-#done
+# send signals to self as test
+echo "current pid is $$ $BASHPID"
+kill -s SIGINT $$
+kill -s SIGUSR1 $$
 
-timeout 5 sleep 1 && true
-echo "exited short sleep ending in true with $? (expected 0)"
 
-timeout 5 sleep 1 && false
-echo "exited short sleep ending in false with $? (expected 1)"
+continue_looping=1
+while [ $continue_looping -eq 1 ]; do
+  echo "Waiting for you to press <CTRL>-C 3 times..."
 
-timeout 2 sleep 10 && $(exit 3)
-echo "exited long sleep ending in false with $? (expected 124 for timeout)"
-timeout 2 sleep 10 && true
-echo "exited long sleep ending in true with $? (expected 124 for timeout)"
+  # send SIGUSR1 every second
+  kill -s SIGUSR1 $$
+  sleep 1
+done
 
-# kill ping after 3 seconds and continue
-echo ""
-echo "ping will die after 3 seconds, then continue processing..."
-timeout 3 ping 127.0.0.1
-echo "exited ping with $? (expected 124 for timeout)"
-
-# disable trap
-#trap "" SIGKILL
-
-# trap 'EXIT' signal (kill) for custom message
-#trap pingtimeout_kill SIGINT
-
-# kill after 3 seconds
-echo ""
-echo "ping will die after 3 seconds with custom message..."
-timeout 3 ping 127.0.0.1
-echo "exited with $?"
-
-#echo ""
-#for i in $(seq 1 3); do
-#  echo "last $i"
-#done
