b216dccdde9ddf390afae453ed640723816f7785
flee999 <flee999@hotmail.com>
2022-05-07 11:03:20 -0400

    blog ref

--- bash/is_script_sourced.sh
+++ bash/is_script_sourced.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # 
 # Tests whether script was invoked as source
+# blog: https://fabianlee.org/2022/05/07/bash-test-whether-script-is-invoked-directly-or-sourced/
 #
 
 # https://stackoverflow.com/questions/2683279/how-to-detect-if-a-script-is-being-sourced
