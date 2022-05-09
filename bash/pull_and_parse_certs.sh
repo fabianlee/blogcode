17e44c3a0c3bd203c772d9f4c3a44e5992a29a4d
flee999 <flee999@hotmail.com>
2022-05-09 16:41:41 -0400

    handles args

--- bash/pull_and_parse_certs.sh
+++ bash/pull_and_parse_certs.sh
@@ -3,7 +3,15 @@
 # Pulls all PEM certs out of yaml file
 # then examines properties using openssl
 
-echo | openssl s_client -showcerts -servername $1 -connect $1:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > $1.crt
+server_name="$1"
+server_ip="$2"
+if [[ -z "$server_name" ]]; then
+  echo "usage: serverFQDN [serverIP]"
+  exit 3
+fi
+[[ -n "$server_ip" ]] || server_ip="$server_name"
+
+echo | openssl s_client -showcerts -servername $server_name -connect $server_ip:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > $1.crt
 
 # pull out multiline sections, remove leading spaces
 sed -ne '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' $1.crt | sed 's/^\s*//' > allcerts.pem
