ea6783a738c41b51d7e6cea90d9caf0690db2709
flee999 <flee999@hotmail.com>
2022-08-06 16:11:49 -0400

    default for computer name

--- windows/rename-host.ps1
+++ windows/rename-host.ps1
@@ -1,3 +1,8 @@
-$name = "win2k19-dc1"
+param([String]$newname = "win2k19-dc1")
 $computerName = Get-WmiObject Win32_ComputerSystem
-$computername.Rename($name)
+$computername.Rename($newname)
+
+write-output "name changed to $newname, going to reboot in 3 seconds..."
+start-sleep -seconds 3
+
+restart-computer
