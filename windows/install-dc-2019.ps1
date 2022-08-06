a83e4ed59496376abaa73cb183f9d6da947c8dd1
flee999 <flee999@hotmail.com>
2022-08-06 16:44:27 -0400

    single quotes for PS

--- windows/install-dc-2019.ps1
+++ windows/install-dc-2019.ps1
@@ -4,27 +4,27 @@
 # https://social.technet.microsoft.com/wiki/contents/articles/52765.windows-server-2019-step-by-step-setup-active-directory-environment-using-powershell.aspx
 # https://petri.com/how-to-install-active-directory-in-windows-server-2019-using-powershell/
 
-read-host "About to add the AD-Domain-Services feature...press <ENTER> to begin"
-Install-WindowsFeature –Name AD-Domain-Services –IncludeManagementTools
+read-host 'About to add the AD-Domain-Services feature...press <ENTER> to begin'
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 start-sleep -seconds 3
 
 Import-Module ADDSDeployment
 
-$domainbios = "FABIAN"
-$domain = "FABIAN.LEE"
-$dmode = "7" # win2019
+$domainbios = 'FABIAN'
+$domain = 'FABIAN.LEE'
+$dmode = '7' # win2019
 
-read-host "About to create the Windows Domain controller for $domain...press <ENTER> to begin"
+read-host 'About to create the Windows Domain controller for $domain...press <ENTER> to begin'
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
--DatabasePath "C:\Windows\NTDS" `
+-DatabasePath 'C:\Windows\NTDS' `
 -DomainMode $dmode `
 -DomainName $domain `
 -DomainNetbiosName $domainbios `
 -ForestMode $dmode `
 -InstallDns:$true `
--LogPath "C:\Windows\NTDS" `
+-LogPath 'C:\Windows\NTDS' `
 -NoRebootOnCompletion:$false `
--SysvolPath "C:\Windows\SYSVOL" `
+-SysvolPath 'C:\Windows\SYSVOL' `
 -Force:$true
 
