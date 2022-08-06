bbf7240b1ec80424377811e9cc5581aebf0d38ab
flee999 <flee999@hotmail.com>
2022-08-06 16:20:25 -0400

    for AD 2019

---
+++ windows/install-dc-2019.ps1
@@ -0,0 +1,18 @@
+#
+# Windows PowerShell script for AD DS Deployment
+#
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "Win2012R2" `
+-DomainName "FABIAN.LEE" `
+-DomainNetbiosName "FABIAN" `
+-ForestMode "Win2012R2" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
