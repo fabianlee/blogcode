31e33d57951722d927ebef302c92c6c02501552b
flee999 <flee999@hotmail.com>
2022-08-06 19:43:33 -0400

    correctly change network config from dynamic to static

--- windows/make-ip-static.ps1
+++ windows/make-ip-static.ps1
@@ -1,19 +1,25 @@
-
+#
+# Sets IP address to static
+#
 # https://adamtheautomator.com/powershell-get-ip-address/
+# https://docs.microsoft.com/en-us/powershell/module/nettcpip/set-netipaddress?view=windowsserver2022-ps
+# https://techexpert.tips/powershell/powershell-configure-static-ip-address/
 
 
 # retrieve network information from what may be dynamically assigned IP
 # if not static IP, then PrefixOrigin=Dhcp SuffixOrigin=Dhcp
-interfaceIndex=(Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).InterfaceIndex
-prefixLen=(Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).PrefixLength
-interfaceIndexAlternate=(get-netadapter).InterfaceIndex
-ipAddress=(Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).IPAddress
+$interfaceIndex=(Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).InterfaceIndex
+$prefixLen=(Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).PrefixLength
+$interfaceIndexAlternate=(get-netadapter).InterfaceIndex
+$ipAddress=(Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).IPAddress
 write-host "The interface index $interfaceIndex/$interfaceIndexAlternate has IP address of $ipAddress"
 
-gateway=(Get-NetIPOConfiguration | select-object -first 1).IPv4DefaultGateway.NextHop
+$gateway=(Get-NetIPConfiguration | select-object -first 1).IPv4DefaultGateway.NextHop
 write-host "default gateway is $gateway"
 
 # make IP address static
+# must remove first, then add new
+Remove-NetIPAddress -InterfaceIndex $interfaceIndex -Confirm:$false
 New-NetIPAddress –IPAddress $ipAddress -DefaultGateway $gateway -PrefixLength $prefixLen -InterfaceIndex $interfaceIndex
 
 write-host "===NEW INFO======================================"
