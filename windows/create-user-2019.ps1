afa022a3491c067ce7c9424616a0512c66cdf271
flee999 <flee999@hotmail.com>
2022-08-06 21:47:46 -0400

    create test user

---
+++ windows/create-user-2019.ps1
@@ -0,0 +1,11 @@
+param([String]$thepass="ThisIsMyP4ss!")
+
+# https://technig.com/create-user-account-using-powershell/
+# https://www.faqforge.com/powershell/create-ad-user-in-specific-ou/
+
+Import-Module activedirectory
+$securePass = ConvertTo-SecureString -string $thepass -asplaintext -force
+
+New-ADUser -Type iNetOrgPerson -Name “jdoe” -GivenName John -Surname Doe -SamAccountName jdoe -UserPrincipalName jdoe@test.local -AccountPassword $securePass -Enabled $true -ChangePasswordAtLogon $false -OtherAttributes @{'title'="engineer";'mail'="jdoe@test.com"}
+
+Add-ADGroupMember -Identity "Remote Desktop Users" -Members jdoe
--- windows/install-dc-2019.ps1
+++ windows/install-dc-2019.ps1
@@ -5,11 +5,11 @@
 # https://petri.com/how-to-install-active-directory-in-windows-server-2019-using-powershell/
 # https://docs.microsoft.com/en-us/powershell/module/addsdeployment/install-addsforest?view=windowsserver2022-ps
 
-read-host 'About to add the AD-Domain-Services feature...press ENTER to begin'
+write-host 'About to add the AD-Domain-Services feature...'
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 start-sleep -seconds 3
 
-read-host 'About to add the in-Services feature...press ENTER to begin'
+write-host 'About to add the DNS feature...'
 Install-WindowsFeature -Name DNS -IncludeManagementTools
 start-sleep -seconds 3
 
@@ -19,9 +19,9 @@ $domainbios = 'FABIAN'
 $domain = 'FABIAN.LEE'
 $dmode = '7' # win2019
 $safepass = 'ThisIsMyP4ss!' # same as example unattend.xml
-$safepassSecure = CONVERTTO-SecureString –asplaintext –force –string $safepass
+$safepassSecure = ConvertTo-SecureString -string $safepass -asplaintext -force
 
-read-host "About to create the Windows Domain controller for $domain...press ENTER to begin"
+write-host "About to create the Windows Domain controller for $domain..."
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath 'C:\Windows\NTDS' `
