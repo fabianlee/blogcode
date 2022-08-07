88094a512fa174022bcd782b3b90ddc6e89025ba
flee999 <flee999@hotmail.com>
2022-08-06 21:57:33 -0400

    operators group

--- windows/create-user-2019.ps1
+++ windows/create-user-2019.ps1
@@ -10,3 +10,7 @@ $securePass = ConvertTo-SecureString -string $thepass -asplaintext -force
 New-ADUser -Type User -Name "jdoe" -GivenName John -Surname Doe -SamAccountName jdoe -UserPrincipalName jdoe@test.local -AccountPassword $securePass -Enabled $true -ChangePasswordAtLogon $false -OtherAttributes @{'title'="engineer";'mail'="jdoe@test.com"}
 
 Add-ADGroupMember -Identity "Remote Desktop Users" -Members jdoe
+
+# one of the groups that will allow a user to login directly to DC
+# https://www.wintips.org/fix-the-sign-in-method-you-are-trying-to-use-is-not-allowed/
+Add-ADGroupMember -Identity "Backup Operators" -Members jdoe
