0eb654ef723721deaca7d8d216e8a29898bd714a
flee999 <flee999@hotmail.com>
2022-08-06 21:01:30 -0400

    removed brackets

--- windows/install-dc-2019.ps1
+++ windows/install-dc-2019.ps1
@@ -5,11 +5,11 @@
 # https://petri.com/how-to-install-active-directory-in-windows-server-2019-using-powershell/
 # https://docs.microsoft.com/en-us/powershell/module/addsdeployment/install-addsforest?view=windowsserver2022-ps
 
-read-host 'About to add the AD-Domain-Services feature...press <ENTER> to begin'
+read-host 'About to add the AD-Domain-Services feature...press ENTER to begin'
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 start-sleep -seconds 3
 
-read-host 'About to add the in-Services feature...press <ENTER> to begin'
+read-host 'About to add the in-Services feature...press ENTER to begin'
 Install-WindowsFeature -Name DNS -IncludeManagementTools
 start-sleep -seconds 3
 
@@ -21,7 +21,7 @@ $dmode = '7' # win2019
 $safepass = 'ThisIsMyP4ss!' # same as example unattend.xml
 $safepassSecure = CONVERTTO-SecureString –asplaintext –force –string $safepass
 
-read-host "About to create the Windows Domain controller for $domain...press <ENTER> to begin"
+read-host "About to create the Windows Domain controller for $domain...press ENTER to begin"
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath 'C:\Windows\NTDS' `
