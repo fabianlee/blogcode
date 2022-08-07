00990a70082fd19b0e6fe1b4104e3c6a1d3b3901
flee999 <flee999@hotmail.com>
2022-08-07 14:56:10 -0400

    could not set password that includes username

--- windows/create-test-users-2019.ps1
+++ windows/create-test-users-2019.ps1
@@ -7,20 +7,21 @@
 # https://devblogs.microsoft.com/scripting/powertip-create-an-active-directory-group-with-powershell/
 # https://www.wintips.org/fix-the-sign-in-method-you-are-trying-to-use-is-not-allowed/
 
+param([String]$password="ThisIsMyP4ss!")
 
 Import-Module activedirectory
 
 # takes user account name, suffixes with 123! and makes into secure password
 function make-secure-pass {
-  param([String]$name)
-  $securePass = ConvertTo-SecureString -string "${name}123!" -asplaintext -force
+  param([String]$plainPass)
+  $securePass = ConvertTo-SecureString -string "$plainPass" -asplaintext -force
   write-output $securePass
 }
 
 # create AD user
 function create-ad-user {
-  param([String]$name,[String]$edomain)
-  New-ADUser -Type User -Name $name -SamAccountName $name -SamAccountName $name -EmailAddress "$name@$edomain" -UserPrincipalName "$name@$edomain" -AccountPassword (make-secure-pass $name) -Enabled $true -ChangePasswordAtLogon $false
+  param([String]$name,[String]$edomain,[String]$password)
+  New-ADUser -Type User -Name $name -SamAccountName $name -EmailAddress "$name@$edomain" -UserPrincipalName "$name@$edomain" -AccountPassword (make-secure-pass "$password") -Enabled $true -ChangePasswordAtLogon $false
 }
 
 
@@ -34,30 +35,30 @@ $edomain="test.local"
 # jdoe - test account that can login directly at console, but not over RDP
 # has additional attributes that can be seen in ADSI
 $name="jdoe"
-New-ADUser -Type User -Name $name -GivenName John -Surname Doe -SamAccountName $name -EmailAddress "$name@$edomain" -UserPrincipalName "$name@$edomain" -AccountPassword (make-secure-pass $name) -Enabled $true -ChangePasswordAtLogon $false -OtherAttributes @{'title'="mytitle";'foo'="bar"}
+New-ADUser -Type User -Name $name -GivenName John -Surname Doe -SamAccountName $name -EmailAddress "$name@$edomain" -UserPrincipalName "$name@$edomain" -AccountPassword (make-secure-pass $password) -Enabled $true -ChangePasswordAtLogon $false -OtherAttributes @{'title'="mytitle";'mail'="$name@$edomain"}
 Add-ADGroupMember -Identity "Remote Desktop Users" -Members $name
 Add-ADGroupMember -Identity "Backup Operators" -Members $name
 
 # myadmin - extra admin account
 $name="myadmin"
-create-ad-user $name $edomain
+create-ad-user $name $edomain "$password"
 Add-ADGroupMember -Identity "Administrators" -Members $name
 
 # adfs1 - service account for ADFS
 $name="adfs1"
-create-ad-user $name $edomain
+create-ad-user $name $edomain "$password"
 
 # engineer1 - test engineer
 $name="engineer1"
-create-ad-user $name $edomain
+create-ad-user $name $edomain "$password"
 Add-ADGroupMember -Identity "engineers" -Members $name
 # engineer2 - another test engineer
 $name="engineer2"
-create-ad-user $name $edomain
+create-ad-user $name $edomain "$password"
 Add-ADGroupMember -Identity "engineers" -Members $name
 
 # manager1 - test manager
 $name="manager1"
-create-ad-user $name $edomain
+create-ad-user $name $edomain "$password"
 Add-ADGroupMember -Identity "managers" -Members $name
 
