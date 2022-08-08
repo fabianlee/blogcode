74e9b30ce0c9cc2b3155f0f5a355459a2e64676f
flee999 <flee999@hotmail.com>
2022-08-08 08:39:41 -0400

    comment added

--- windows/install-adfs-2019.ps1
+++ windows/install-adfs-2019.ps1
@@ -6,7 +6,7 @@ Import-Module ADFS
 $theDomain="$env:USERDOMAIN"
 $userDNSDomain="$env:USERDNSDOMAIN"
 
-# do NOT prompt
+# do NOT prompt manually
 #$installationCredential = Get-Credential -Message "Enter credentials of user to perform config"
 #$serviceAccountCredential = Get-Credential -Message "Enter credential for federation service acct"
 
@@ -18,8 +18,8 @@ $serviceAccountCredential = New-Object System.Management.Automation.PSCredential
 $leafThumbprint=(get-ChildItem -Path 'Cert:\LocalMachine\My' | where-object { $_.Subject -like 'CN=win2k19-adfs1*' }).Thumbprint
 write-host "leafThumbprint is $leafThumbprint"
 
-# going to use our cert for: service, token-decrypt, and token-signing
-# otherwise we would need to get the generated tokens and add to 'Root' store
+# going to use our custom cert for: service, token-decrypt, and token-signing
+# otherwise we would need to fetch the generated tokens and add to 'Root' store
 #      get-adfscertificate -certificateType token-signing
 #      get-adfscertificate -certificateType token-decrypting
 Install-AdfsFarm `
@@ -27,7 +27,7 @@ Install-AdfsFarm `
 -SigningCertificateThumbprint "$leafThumbprint" `
 -DecryptionCertificateThumbprint "$leafThumbprint" `
 -Credential $installationCredential `
--FederationServiceDisplayName "myadfs.$userDNSDomain" `
+-FederationServiceDisplayName "My win2k19-adfs1.$userDNSDomain" `
 -FederationServiceName "win2k19-adfs1.$userDNSDomain" `
 -ServiceAccountCredential $serviceAccountCredential `
 -OverwriteConfiguration `
