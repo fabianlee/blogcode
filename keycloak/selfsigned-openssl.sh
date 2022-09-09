e6ccbc7f15f154daec80144cdde86e39fa6af05a
flee999 <flee999@hotmail.com>
2022-09-09 18:18:21 -0400

    for creating self-signed cert

---
+++ keycloak/selfsigned-openssl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FQDN="$1"
+[ -n "$FQDN" ] || { echo "ERROR provide FQDN for self-signed cert"; exit 3; }
+
+sudo apt-get install libssl1.0.0 -y
+
+echo -------------------
+echo FQDN is $FQDN
+echo -------------------
+
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
+-keyout /tmp/$FQDN.key -out /tmp/$FQDN.pem \
+-subj "/C=US/ST=CA/L=SFO/O=myorg/CN=$FQDN"
+
+openssl x509 -in /tmp/$FQDN.pem -text -noout | grep -E "Subject:|Not After :|DNS:|Issuer:"
+
+echo ""
+echo "public cert and private key are located in /tmp directory"
+
