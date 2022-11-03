d6e66dbfaa6ee83589bb966f685da7352299de2d
Fabian Lee <flee999@hotmail.com>
2022-11-02 20:30:22 -0400

    one liner for testing multiple values

--- bash/test_value_choices.sh
+++ bash/test_value_choices.sh
@@ -9,13 +9,23 @@ myval="$1"
 [ -n "$myval" ] || { echo "ERROR please provide a parameter at the command line"; exit 1; }
 echo "myval = $myval"
 
+# one liner
+#[[ "$myval" == @("on"|"off") ]] || { echo "ERROR does not match"; exit 4; }
+
 # if statement
 #if [[ ! "$myval" == @("on"|"off") ]]; then
-#  echo "ERROR value must either be 'on' or 'off'"; exit 2;
+#  echo "ERROR value must either be 'on' or 'off'"; exit 3;
 #fi
 
-# one liner
-[[ "$myval" == @("on"|"off") ]] || { echo "ERROR does not match"; exit 2; }
+# use case statement
+#case $myval in
+#on|off)
+#  ;;
+#*)
+#  echo "ERROR does not match"
+#  exit 5;
+#  ;;
+#esac
 
 
 echo "SUCCESS value was '$myval'"
