09f3c6efe1b7955b674ad4c908b9eb18aa3c0e09
Fabian Lee <flee999@hotmail.com>
2023-01-25 15:50:06 -0500

    using sqlite3 to transfer gcloud credentials

---
+++ gcloud/grestore.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# Processes output of 'gsave.sh'
+# Inserts credentials into gcloud configs
+#
+
+function decrypt_with_openssl() {
+  text="$1"
+  # base64 encoding was applied twice:
+  # 1. once to get rid of special chars/unicode and create single line of text to encode
+  # 2. another time to turn encrypted binary into ascii-friendly text for copy-paste
+  echo "$text" | base64 -d | openssl enc -d -aes-256-cbc -pbkdf2 -iter 1234567 -salt -pass pass:abc123 | base64 -d
+} 
+
+function ensure_binary() {
+  thebinary=$1
+  themsg="$2"
+
+  findbinary=$(which $thebinary)
+  [[ -n "$findbinary" ]] || { echo "ERROR could not find binary $thebinary, $themsg"; exit 3; }
+
+}
+ensure_binary gcloud "install gcloud, https://cloud.google.com/sdk/docs/install"
+ensure_binary sqlite3 "use 'apt install sqlite3 -y' to install sqlite"
+ensure_binary base64 "use 'apt install coreutils -y' to install base64"
+ensure_binary base64 "use 'apt install openssl -y' to install openssl"
+
+# input comes as one line of encrypted text, base64 encoded
+read -r stdin_line
+decrypted_lines=$(decrypt_with_openssl $stdin_line)
+#echo "decrypted=$decrypted_lines"
+
+COUNT=0
+IFS=$'\n'
+for line in $decrypted_lines; do
+
+  # throw out boundary lines if user accidently included
+  if [[ "$line" =~ =====COPY ]]; then continue; fi
+  # skip empty lines
+  [[ -n "$line" ]] || continue
+  #echo "processing line $COUNT: $line"
+
+  case $COUNT in
+  0)
+    current_user=$line
+    [[ -n "$current_user" ]] || { echo "ERROR could not retrieve target gcloud user"; exit 3; }
+    echo -e "\n"
+    echo "===RESTORE for $current_user"
+    ;;
+  1)
+    if [ "$line" == "n/a" ]; then
+      echo "No access_tokens to restore for $current_user"
+    else
+      sqlite3 ~/.config/gcloud/access_tokens.db "delete from access_tokens where account_id='$current_user'"
+      echo $line | sqlite3 ~/.config/gcloud/access_tokens.db ".import \"|cat -\" access_tokens"
+      echo "INSERT ~/.config/gcloud/access_tokens.db: $?"
+    fi
+    ;;
+  2)
+    if [ "$line" == "n/a" ]; then
+      echo "No credentials to restore for $current_user"
+    else
+      sqlite3 ~/.config/gcloud/credentials.db "delete from credentials where account_id='$current_user'"
+      echo $line | sqlite3 ~/.config/gcloud/credentials.db
+      echo "INSERT ~/.config/gcloud/credentials.db: $?"
+    fi
+
+    ;;
+  *)
+    true
+    #echo "ERROR only first 3 non-empty lines are valid (id,access_tokens,credentials)"
+  esac
+
+  ((COUNT+=1))
+done < <(echo $all_lines)
+
+
+# throw away any extra lines of stdin
+if test -t 0; then
+  #echo "going to eat the rest of stdin"
+  while read -r -t 1 foo; do true; done
+fi
---
+++ gcloud/gsave.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# Dumps credentials of current gcloud user from configs
+# The output can be used to run 'grestore.sh' from another host and use the same credentials
+#
+
+function encrypt_with_openssl_tobase64() {
+  text="$1"
+  # using OS level base64 which can output full width of screen
+  # avoid 3des weak cipher
+  echo "$text" | openssl enc -aes-256-cbc -pbkdf2 -iter 1234567 -salt -pass pass:abc123 | base64 -w0
+} 
+
+function ensure_binary() {
+  thebinary=$1
+  themsg="$2"
+
+  findbinary=$(which $thebinary)
+  [[ -n "$findbinary" ]] || { echo "ERROR could not find binary $thebinary, $themsg"; exit 3; }
+
+}
+ensure_binary gcloud "install gcloud, https://cloud.google.com/sdk/docs/install"
+ensure_binary sqlite3 "use 'apt install sqlite3 -y' to install sqlite"
+ensure_binary base64 "use 'apt install coreutils -y' to install base64"
+ensure_binary base64 "use 'apt install openssl -y' to install openssl"
+
+current_user=$(gcloud auth list 2>/dev/null | grep "^*" | cut -d'*' -f2 | tr -d ' ')
+[[ -n "$current_user" ]] || { echo "ERROR could not retrieve gcloud current user"; exit 3; }
+echo "Exporting encrypted credentials for $current_user, use grestore.sh to import" >&2
+echo "" >&2
+
+access_output=$(sqlite3 ~/.config/gcloud/access_tokens.db "select * from access_tokens where account_id='$current_user'")
+[[ -n "$access_output" ]] || access_output="n/a"
+
+credentials_output=$(sqlite3 ~/.config/gcloud/credentials.db ".dump" | grep "VALUES('$current_user',")
+[[ -n "$credentials_output" ]] || credentials_output="n/a"
+
+# create multi-line paragraph
+# 1st line=user id, 2nd line=data dump from access | seperator,3rd line=sql insert stmt from credentials
+read -r -d '' to_encrypt_stringb64 <<EOF
+$current_user
+$access_output
+$credentials_output
+EOF
+# make single line without special chars/unicode
+to_encrypt_stringb64=$(echo "$to_encrypt_stringb64" | base64 -w0)
+
+# the encryption itself puts a base64 wrapping also, so we can-copy paste encrypted text easily (not binary)
+# You could call this 'double wrapping' inefficient, but it avoids special char/unicode edge cases
+for_user_to_copy=$(encrypt_with_openssl_tobase64 $(echo "$to_encrypt_stringb64"))
+
+# this is what users would copy-paste
+echo "=====COPY EVERYTHING AFTER THIS MARKER=====" >&2
+echo "$for_user_to_copy"
+echo ""
+echo "=====COPY EVERYTHING BEFORE THIS MARKER=====" >&2
+
