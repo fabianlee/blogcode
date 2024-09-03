7a2f7b2512fc2f16db36cfc6f624ba7138ce2b84
Fabian Lee <flee999@hotmail.com>
2024-09-03 07:14:49 -0400

    added documentation

--- terraform/yaml_contribution_model/main.tf
+++ terraform/yaml_contribution_model/main.tf
@@ -1,5 +1,5 @@
 #
-# blog: 
+# blog: https://fabianlee.org/2024/09/01/terraform-external-yaml-file-as-a-contribution-model-for-outside-teams/
 #
 
 locals {
@@ -8,19 +8,20 @@ locals {
     { name="foo4", content="this is foo4" }
   ]
 
-  file_data_ext_yaml = yamldecode(file("${path.module}/external.yaml"))
-  #file_data_ext_yaml = fileexists("${path.module}/external.yaml") ? yamldecode(file("${path.module}/external.yaml")):{ files= [] }
+  #file_data_ext_yaml = yamldecode(file("${path.module}/external.yaml"))
+  file_data_ext_yaml = fileexists("${path.module}/external.yaml") ? yamldecode(file("${path.module}/external.yaml")):{ files= [] }
 
   file_data_merge = concat( local.file_data_static, local.file_data_ext_yaml.files )
 }
 
+# if hard-coding resources
 #resource "local_file" "foo_test" {
 #  for_each = { for entry in local.file_data : entry.name=>entry }
 #  content  = each.value.content
 #  filename = "${path.module}/${each.value.name}.txt"
 #}
 
-
+# for data-driven resources
 resource "local_file" "foo" {
   #for_each = { for entry in local.file_data_ext_yaml.files : entry.name=>entry }
   for_each = { for entry in local.file_data_merge : entry.name=>entry }
@@ -28,6 +29,7 @@ resource "local_file" "foo" {
   filename = "${path.module}/${each.value.name}.txt"
 }
 
-output "show_ext_yaml_files" {
-  value = local.file_data_ext_yaml.files
+output "show_data" {
+  value = local.file_data_merge
+  #value = flatten([for file in local.file_data_merge: file.name ])
 }
