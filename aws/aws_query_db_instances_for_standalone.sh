5ca149f5224a55bd9472c11d1e4f91457fad5f4b
Fabian Lee <flee999@hotmail.com>
2026-01-12 07:55:58 -0500

    better output for end user

--- aws/aws_query_db_instances_for_standalone.sh
+++ aws/aws_query_db_instances_for_standalone.sh
@@ -4,12 +4,16 @@
 # OR alternatively using jq to filter
 #
 
-echo "using aws cli only, show DB instances that are standalone and not part of Cluster membership"
+echo "===== DB Instances that are standalone, no cluster membership"
+
+echo "using aws cli only, show DB instances that are standalone"
 aws rds describe-db-instances --no-cli-pager --query "DBInstances[?DBClusterIdentifier==null].DBInstanceIdentifier" --output text
-echo "using aws cli and jq, show DB instances that are standalone and not part of Cluster membership"
+echo "using aws cli and jq, show DB instances that are standalone"
 aws rds describe-db-instances --no-cli-pager | jq '.DBInstances[] | select(has("DBClusterIdentifier")|not).DBInstanceIdentifier' -r
+
+echo "=====DB Instances that are member of cluster"
 
 echo "using aws cli only, show DB instances that are part of Cluster membership"
 aws rds describe-db-instances --no-cli-pager --query "DBInstances[?DBClusterIdentifier!=null].DBInstanceIdentifier" --output text
-echo "using aws cli and jq, show DB instnces that are part of Cluster membership"
+echo "using aws cli and jq, show DB instances that are part of Cluster membership"
 aws rds describe-db-instances --no-cli-pager | jq '.DBInstances[] | select(has("DBClusterIdentifier")).DBInstanceIdentifier' -r
